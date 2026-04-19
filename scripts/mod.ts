db414c305c1c6b27d07bbf0374feb35c19ff76a7
xanedesu <xanedesu@gmail.com>
2026-04-19 16:10:48 +0300

    feat: implement init, publish, install, and remove functions in mod.ts

--- scripts/mod.ts
+++ scripts/mod.ts
@@ -1,13 +1,69 @@
+import assert from 'node:assert';
 import { glob, readFile, writeFile } from 'node:fs/promises';
 import { relative, resolve } from 'node:path';
 import { parseArgs } from 'node:util';
 
-async function install(args: string[]) {}
+const modlock = '.modlock.json';
+const modrc = '.modrc.json';
+const module = 'module.json';
 
-async function uninstall(args: string[]) {}
+interface Mod {
+  name: string;
+  version: string;
+}
+
+interface Modlock {
+  [mod: string]: Mod & {
+    dependencies: Modlock;
+  };
+}
+
+interface Modrc {
+  registry: string;
+}
+
+async function init(args: string[]) {
+  const { values } = parseArgs({
+    strict: true,
+    options: {
+      registry: {
+        type: 'string'
+      },
+      rootDir: {
+        type: 'string',
+        default: 'src'
+      }
+    },
+    args
+  });
+
+  assert(values.registry, 'registry is required');
+
+  await Promise.all([
+    writeFile(
+      resolve(values.rootDir, 'modules', modrc),
+      JSON.stringify(
+        {
+          registry: values.registry
+        } satisfies Modrc,
+        undefined,
+        2
+      )
+    ),
+    writeFile(
+      resolve(values.rootDir, 'modules', modlock),
+      JSON.stringify({} satisfies Modlock)
+    )
+  ]);
+}
+
+async function publish(args: string[]) {
+  throw new Error('not implemented');
+}
 
 async function build(args: string[]) {
   const { values } = parseArgs({
+    strict: true,
     options: {
       rootDir: {
         type: 'string',
@@ -22,7 +78,7 @@ async function build(args: string[]) {
   });
 
   for await (const mod of glob(
-    resolve(values.rootDir, 'modules', '*', 'module.json')
+    resolve(values.rootDir, 'modules', '*', module)
   )) {
     const file = await readFile(mod, 'utf8');
     const json = JSON.parse(file) as {
@@ -40,17 +96,31 @@ async function build(args: string[]) {
   }
 }
 
+async function install(args: string[]) {
+  throw new Error('not implemented');
+}
+
+async function remove(args: string[]) {
+  throw new Error('not implemented');
+}
+
 const [command, ...args] = process.argv.slice(2);
 switch (command) {
-  case 'install':
-    await install(args);
+  case 'init':
+    await init(args);
     break;
-  case 'uninstall':
-    await uninstall(args);
+  case 'publish':
+    await publish(args);
     break;
   case 'build':
     await build(args);
     break;
+  case 'install':
+    await install(args);
+    break;
+  case 'remove':
+    await remove(args);
+    break;
   default:
-    throw new Error('unknown command');
+    throw new Error('unknown');
 }
