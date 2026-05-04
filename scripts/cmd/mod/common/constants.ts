ef6841b1adc31e2508fcb9fa5aab4c46d3202aa1
xanedesu <xanedesu@gmail.com>
2026-05-04 22:07:17 +0300

    feat: implement create function for module generation with validation

--- scripts/cmd/mod/common/constants.ts
+++ scripts/cmd/mod/common/constants.ts
@@ -1,3 +1,5 @@
 export const MODLOCK = 'modlock.json';
 export const MODRC = 'modrc.json';
 export const MODULE = 'module.json';
+
+export const MODULE_NAME = /^[a-z0-9][a-z0-9-]*$/;
--- scripts/cmd/mod/create.ts
+++ scripts/cmd/mod/create.ts
@@ -1,3 +1,35 @@
+import assert from 'node:assert';
+import { mkdir, writeFile } from 'node:fs/promises';
+import { resolve } from 'node:path';
+import { parseArgs } from 'node:util';
+import { MODULE, MODULE_NAME } from './common/constants.ts';
+import { createTsconfigs } from './common/helpers/tsconfig.ts';
+import type { Mod } from './common/types.ts';
+
 export async function create(args: string[]) {
-  throw new Error('not implemented');
+  const { positionals } = parseArgs({
+    strict: true,
+    allowPositionals: true,
+    args
+  });
+
+  assert(positionals.length === 1, 'module name is required');
+
+  const [name = ''] = positionals;
+  assert(MODULE_NAME.test(name), `${name}: invalid module name`);
+
+  const root = resolve('src', 'modules', name);
+  const mod = {
+    name,
+    version: '0.1.0'
+  } satisfies Pick<Mod, 'name' | 'version'>;
+
+  await mkdir(root);
+  await writeFile(resolve(root, MODULE), JSON.stringify(mod, undefined, 2));
+  await createTsconfigs([
+    {
+      root,
+      name
+    }
+  ]);
 }
